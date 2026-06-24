8c04e220d09ba55fe1701e8c0fc7c6e890cc7eed
psychwangzihao <1022182060@qq.com>
2026-06-24 07:09:59 +0000

    Deploying to gh-pages from @ Consciousness-Observers/consciousness-observers.github.io@0b3a19317b8ce5fa947962ed7228f76acf0e8549 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-home",
-          title: "home",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-members",
+  },{id: "nav-members",
           title: "members",
           description: "",
           section: "Navigation",
